00ed86adb4b7147fcb0ded35378841840485d99f
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-26 21:36:23 +0200

    Update shema - add subdocument (movieSchema)

--- src/shema.js
+++ src/shema.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
  
+const movieSchema = new Schema(
+  {
+    title: String,
+    year: Number,
+    format: String,
+    stars: [String]
+  }
+)
+
 const AccountSchema = new Schema(
   {
     username: {
@@ -14,11 +23,11 @@ const AccountSchema = new Schema(
       type: String,
       required: [true, "Required!"],
     },
-    movies: [Object]
+    movies: [movieSchema]
   }
-);
+)
 
 
 module.exports = (mongoose.models && mongoose.models.Account)
 ? mongoose.models.Account
-: mongoose.model('Account', AccountSchema);
+: mongoose.model('Account', AccountSchema)
