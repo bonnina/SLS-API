1c8af268cde44c6026c9f47e71b68488424a3993
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-27 22:25:50 +0200

    Add file upload route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const fs = require("fs")
+const jwt = require('jsonwebtoken')
 const Account = require('../shema')
 const registerUser = require('../methods/registerUser')
 const logInUser = require('../methods/logInUser')
-const jwt = require('jsonwebtoken')
 
 router.post('/signup', (req, res, next) => { 
   registerUser(req.body)
-  .then(result => {
-    res.send(result)
-  })
-  .catch(err =>
-    res(err.statusCode || 500).send(err.message)
-  )
+  .then(result => res.send(result))
+  .catch(err => res(err.statusCode || 500).send(err.message))
 })
 
 router.post('/login', (req, res, next) => {
   logInUser(req.body)
-  .then(result => {
-    res.status(201).send(result)
-  })
-  .catch(err =>
-    res(err.statusCode || 500).send(err.message)
-  )
+  .then(result => res.status(201).send(result))
+  .catch(err => res(err.statusCode || 500).send(err.message))
 })
 
 router.put('/:id', (req, res, next) => {
@@ -36,25 +29,64 @@ router.put('/:id', (req, res, next) => {
       if (err)
         reject(new Error('Token is not valid'))
         
-      resolve(Account.findByIdAndUpdate(
-        req.params.id,
-        { $set: { movies: req.body.movies }}, 
-        { $upsert: true, new: true }) 
-      )
+      resolve(Account.findByIdAndUpdate(req.params.id, { $set: { movies: req.body.movies }}, { $upsert: true, new: true }))
     })
   })
   .then(acc => ({
     success: true,
-    err: null,
-    token: token,  
+    err: null,  
     movies: acc.movies
   }))
-  .then(result => 
-    res.status(200).send(result)
-  )
-  .catch(err => 
-    res.status(err.statusCode || 401).send(err.message)
-  );
+  .then(result => res.status(200).send(result))
+  .catch(err => res.status(err.statusCode || 401).send(err.message))
+})
+
+router.post('/:id', function (req, res, next) {
+  if (!req.files)
+    res.status(400).send('No files were uploaded.')
+  else {
+    const file = req.files.file;
+  
+    return new Promise((resolve, reject) => {
+      file.mv('./movies.txt', function (err) {
+        if (err) return res.status(500).send('error')
+        resolve();
+      })
+    })
+      .then(() => {
+        fs.readFile('./movies.txt', 'utf8', function (error, data) {
+          if (error) throw error;
+
+          const str = data.toString();
+          const arr = str.split('\n\n').map(el => el.split('\n'));
+        
+          let objectify = function (el) {
+            return new Promise((resolve) => {
+              let movie = el.map(line => line.split(': '));
+              resolve({
+                "title": movie[0][1],
+                "year": movie[1][1],
+                "format": movie[2][1],
+                "stars": [...movie[3][1].split(', ')]
+              })
+            })
+          };
+
+          let actions = arr.map(objectify);
+          Promise.all(actions)
+          .then(data => 
+            Account.findByIdAndUpdate( req.params.id, { $set: { movies: data }}, { $upsert: true, new: true }) 
+          )
+          .then(acc => ({
+            success: true,
+            err: null,  
+            movies: acc.movies
+          }))
+          .then(result => res.status(200).send(result))
+        })
+      })
+      .catch(err => res.status(err.statusCode || 401).send(err.message))
+    }
 })
 
 router.delete('/:id', (req, res, next) => {
@@ -74,8 +106,7 @@ router.delete('/:id', (req, res, next) => {
   .then(() => {
     res.status(200).send({
       success: true,
-      err: null,
-      token: token, 
+      err: null, 
       movies: []
     }) 
   })
