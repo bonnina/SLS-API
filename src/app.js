5b0f5ca0048dc4fed4bdffd4bda78e5478d51f89
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-27 22:25:14 +0200

    Add fileUpload

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
 const routes = require('./routes')
@@ -8,6 +9,7 @@ require('./db')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet())
+app.use(fileUpload())
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.header("Access-Control-Allow-Headers", "Content-Type");
